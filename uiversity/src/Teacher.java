fde47bcac8c0e000c8fc9c9dd4b8f6e35aadbbf1
norivlad <sedich.vlad@rambler.ru>
2024-11-26 01:04:27 +0300

    Create Teacher.java

---
+++ uiversity/src/Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher {
+    private int id;
+    private String fullName;
+
+    public Teacher(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
